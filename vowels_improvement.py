5a5a3f646089371664df199ecb155d3865dd95e3
lumaks <94898801+Lumaks42@users.noreply.github.com>
2023-05-31 09:35:58 +0300

    Create vowels_improvement.py
    add code in programm #5

--- vowels_improvement.py
+++ vowels_improvement.py
@@ -1,1 +1,17 @@
+# Вывести кол-во гласных букв в предложекние введеных пользователем.
+vowels = ['a', 'e', 'i', 'o', 'u']                        # список гласных букв
+word = input("Provide a word to search for vowels: ")     # запрос предложения, слова
+found = {}                                                # словарь
 
+found['a'] = 0
+found['e'] = 0
+found['i'] = 0
+found['o'] = 0
+found['u'] = 0
+
+for letter in word:                                       # перебор букв в слове
+	if letter in vowels:                                    # если буква глассная
+		found[letter] += 1                                    # +1 буква
+
+for k, v in sorted(found.items()):                        # ключ и значение 
+	print(k, 'was found', v, 'time(s).')                    # буква ее кол-во
